5e7ca625d5cd268b7f52b4c192f04e29301f8d53
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-04-26 21:14:16 -0700

    Milestone 2

--- car.cpp
+++ car.cpp
@@ -3,15 +3,21 @@
 #include <string>
 using namespace std;
 
+const int SWITCH = 15;
+const int JOIN = 39;
+const int PAY = 46;
 const int INITIAL_SIZE = 2;
+const int LANES = 4;
 
-void add_Car(deque<Car> &);
-void print_q(deque<Car> &);
+void add_Car(deque<deque<Car>> &);
+void print_q(deque<deque<Car>> &);
 
 int main(){
     srand(time(0));
-    // declare dequeue
-    deque<Car> line(INITIAL_SIZE);
+
+    //declare dequeue for the lanes 
+    deque<deque<Car>> lanes(LANES);
+
     cout << "Initial Queue:\n";
     print_q(line);
 
@@ -48,13 +54,13 @@ int main(){
     return 0;
 }
 
-void add_Car(deque<Car> &l){
+void add_Car(deque<deque<Car>> &l){
     // pushing new car to the deque
     Car n = Car();
     l.push_back(n);
 }
 
-void print_q(deque<Car> &l){
+void print_q(deque<deque<Car>> &l){
     for (int i = 0; i < l.size(); i++){
         cout << "\t";
         l[i].print();
